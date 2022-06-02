a888e4fff930d7b8b3110e62eff5f57774c12078
lu-soto <90468614+lu-soto@users.noreply.github.com>
2022-06-02 12:04:45 -0300

    Update PageConstants.java

--- src/main/java/constants/PageConstants.java
+++ src/main/java/constants/PageConstants.java
@@ -2,9 +2,9 @@ package constants;
 
 public class PageConstants {
 
-    public static final String USERNAME = "(//input[contains(@class,'inputBox input')])[1]";
-    public static final String PASSWORD = "(//input[contains(@class,'inputBox input')])[2]";
-    public static final String LOGIN_BTN =  "//span[@class=' label bBody'][contains(.,'Entrar')]";
+    public static final String USERNAME = "//input[@placeholder='Username']";
+    public static final String PASSWORD = "//input[@placeholder='Password']";
+    public static final String LOGIN_BTN =  "//span[contains(.,'Log in')]";
     public static final String LOGO = "(//div[@class='logoImage'])";
 
 }
