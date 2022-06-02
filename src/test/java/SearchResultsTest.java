06ceb3f0eeb1298905e07dfb6caed7023053b8ad
lu-soto <90468614+lu-soto@users.noreply.github.com>
2022-06-02 12:25:57 -0300

    Update SearchResultsTest.java

--- src/test/java/SearchResultsTest.java
+++ src/test/java/SearchResultsTest.java
@@ -3,23 +3,32 @@ import elements.LoginPage;
 import org.openqa.selenium.WebDriver;
 import elements.*;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchResultsTest extends BaseTest{
 
     @Test
-    public void testFilters() throws InterruptedException {
+    public void verifyProductSearch() throws InterruptedException {
         WebDriver driver = getDriver();
-        LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
         SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
 
         util.waitForPageToLoad(5000);
         homePage.searchItem();
-        util.waitForPageToLoad(15000);
-        searchResultsPage.agregarFiltro();
-        util.waitForPageToLoad(5000);
-        searchResultsPage.liquidacionIsDisplayed();
-
     }
+    @Test
+    public void verifyAddFilter() throws InterruptedException {
+        WebDriver driver = getDriver();
+        HomePage homePage = new HomePage(driver);
+        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
+
+        util.waitForPageToLoad(3000);
+        homePage.searchItem();
+        util.waitForPageToLoad(3000);
+        searchResultsPage.agregarFiltro();
+        util.waitForPageToLoad(3000);
+        Assert.assertTrue(searchResultsPage.getFilter().isEnabled());
+    }
+
 }
