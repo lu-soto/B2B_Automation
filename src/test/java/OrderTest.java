feab484d2f725bcce9c38e85f29c7ab35308e30f
lu-soto <90468614+lu-soto@users.noreply.github.com>
2022-06-02 12:25:05 -0300

    Update OrderTest.java

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,105 +1,79 @@
 import elements.*;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
-public class OrderTest extends BaseTest {
+public class QuickOrderTest extends BaseTest {
 
     @Test (priority=1)
-    public void verifyE2E() throws InterruptedException {
+    public void quickOrderVerCarrito () throws Exception {
         WebDriver driver = getDriver();
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
-        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
-        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
-        DeliveryPage deliveryPage = new DeliveryPage(driver);
-        CheckOutSummaryPage checkOutSummaryPage = new CheckOutSummaryPage(driver);
-        PaymentPage paymentPage = new PaymentPage(driver);
-
-        homePage.searchItem();
-        util.waitForPageToLoad(5000);
-        searchResultsPage.addToCart();
-        util.waitForPageToLoad(10000);
-        searchResultsPage.clickViewCart();
-        driver.switchTo().activeElement();
-        util.waitForPageToLoad(3000);
-        shoppingCartPage.clickCheckOut();
-        util.waitForPageToLoad(3000);
-        deliveryPage.clickNext();
-        util.waitForPageToLoad(3000);
-        checkOutSummaryPage.clickNext();
-        util.waitForPageToLoad(5000);
-        paymentPage.enterPO();
-        util.waitForPageToLoad(3000);
-        Assert.assertEquals(paymentPage.message.getText(), "Thanks for shopping with us!");
-        System.out.println("Se concretó la compra");
-}
-
-    @Test (priority=2)
-    public void verProducto () throws InterruptedException {
-        WebDriver driver = getDriver();
-        HomePage homePage = new HomePage(driver);
-        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
-
-        util.waitForPageToLoad(3000);
-        homePage.searchItem();
-        util.waitForPageToLoad(5000);
-        //searchResultsPage.verProducto();
-        //Validación que es rediriido a la página de detalle del producto
-        Assert.assertEquals(driver.getTitle(), "Product Detail");
-    }
-    @Test(priority=3)
-    public void verAplicacionesVehiculares () throws InterruptedException {
-        WebDriver driver = getDriver();
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
-        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
-
-        util.waitForPageToLoad(3000);
-        homePage.searchItem();
-        util.waitForPageToLoad(5000);
-        //searchResultsPage.verAplicaciones();
-        driver.switchTo().activeElement();
-        //Validacion que muestra que se están mostrando las aplicaciones vehiculares
-        //Assert.assertEquals(searchResultsPage.getApModalTitle().getText(), "Aplicaciones de este repuesto");
-    }
-
-    @Test(priority=4)
-    public void verCrossReference () throws InterruptedException {
-        WebDriver driver = getDriver();
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
-        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
-
-        util.waitForPageToLoad(3000);
-        homePage.searchItem();
-        util.waitForPageToLoad(5000);
-       // searchResultsPage.verCrossReference();
-        driver.switchTo().activeElement();
-        //Validacion que muestra que se está las referencias cruzadas
-       //Assert.assertEquals(searchResultsPage.getCrossModalTitle().getText(), "Cross Reference");
-    }
-    @Test (priority=5)
-    public void validarIva () throws Exception {
-        WebDriver driver = getDriver();
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
+        QuickOrderPage quickOrder = new QuickOrderPage(driver);
         CartPage cart = new CartPage(driver);
-        SearchResultsPage searchResults = new SearchResultsPage(driver);
         CheckOutSummaryPage summaryPage = new CheckOutSummaryPage(driver);
         CompraCompletadaPage compraCompletadaPage = new CompraCompletadaPage(driver);
 
+
         util.waitForPageToLoad(3000);
-        homePage.searchItem();
-        util.waitForPageToLoad(5000);
-        searchResults.addToCart();
-       // searchResults.irAlCarrito();
-        cart.calculoIva();
-        //Valida que el el iva mostrado es correcto
-        String iva = "$" + cart.calculoIva();
-        Assert.assertEquals(cart.getIvaValue().getText(), String.format("$%(,.0f", cart.calculoIva()).replace(",", "."));
-        Assert.assertEquals(cart.getTotalCarro().getText(),
-                String.format("$%(,.0f", cart.calculoTotalCarro(cart.totalNeto(), cart.calculoIva())));
+        quickOrder.searchSKU1();
+        //quickOrder.searchSKU2();
+        quickOrder.addCantidad();
+        quickOrder.getAddToCartBtn().click();
+        driver.switchTo().activeElement();
+        quickOrder.modalVerCarrito();
+        //Valida que el usuario es redirigido a la página del carrito
+        Assert.assertEquals(driver.getTitle(),"Cart");
+        cart.clickComprarCarro();
+        summaryPage.clickSiguiente();
+        summaryPage.seleccionarTransporte();
+        summaryPage.clickCalcularFecha();
+        //util.scrollToViewElement(driver, driver.findElement(By.xpath("//button[contains(.,'Siguiente')]")));
+        summaryPage.unFocusPoInput();
+        summaryPage.clickSiguiente();
+        //Valida el mensaje de éxito en la compra
+        Assert.assertEquals(compraCompletadaPage.getGraciasMsj().getText(), "¡Gracias por comprar con nosotros!");
+        //Assert.assertEquals(compraCompletadaPage.getPedidoCompletoMsj().getText(),"");
+        System.out.println("Se concretó la compra rápida");
+    }
+    @Test (priority=2)
+    public void quickOrderSeguirComprando () throws InterruptedException {
+        WebDriver driver = getDriver();
+        QuickOrderPage quickOrder = new QuickOrderPage(driver);
+
+        quickOrder.searchSKU1();
+        quickOrder.getAddToCartBtn().click();
+        driver.switchTo().activeElement();
+        quickOrder.modalSeguirComprando();
+        //Valida que el usuario es redirigido a la página de home para seguir comprando
+        Assert.assertEquals(driver.getTitle(),"Home");
+    }
+    @Test (priority=3)
+    public void quickOrderAgregarLinea () throws InterruptedException {
+        WebDriver driver = getDriver();
+        LoginPage loginPage = new LoginPage(driver);
+        QuickOrderPage quickOrder = new QuickOrderPage(driver);
+
+        util.waitForPageToLoad(3000);
+        quickOrder.getAgregarLineaBtn().click();
+        System.out.println("Se agregó una línea");
+    }
+    @Test(priority=4)
+    public void borrarCarrito () throws InterruptedException {
+        WebDriver driver = getDriver();
+        QuickOrderPage quickOrder = new QuickOrderPage(driver);
+        CartPage cart = new CartPage(driver);
+
+        quickOrder.searchSKU1();
+        quickOrder.getAddToCartBtn().click();
+        driver.switchTo().activeElement();
+        quickOrder.modalVerCarrito();
+        cart.borrarCarrito();
+        driver.switchTo().activeElement();
+        cart.modalBorrarCarrito();
+        //Validación de que el carro se vació
+        Assert.assertEquals(cart.getVacioMsj().getText(),"Tu carro está vacío");
+        System.out.println("Se vacio el carro");
+
     }
 
 }
